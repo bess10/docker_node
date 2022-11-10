76e0675f975877b52469d0ef27adb6c8142f4e61
Bessy <salgadobessy10@gmail.com>
2022-11-10 16:12:15 +0100

    añadiendo fichero de inicio index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 //Aquí arrancaremos el servidor
+const express = require('express');
+const app = require('./app.js');
+//app.use(express.static('public'));
+// const router = require('./routes/route.js');
+// app.use(router);
 
+app.listen(4000, () => console.log('Server running...'));
